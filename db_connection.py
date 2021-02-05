6da54a1170152ebb47edc31b7b0e754fbb98b322
Vittorio Rivabella <20019426@uhi.ac.uk>
2021-02-05 02:48:26 +0100

    retrieve all ducks infos from ducks shop

--- db_connection.py
+++ db_connection.py
@@ -10,10 +10,17 @@ db_uri = 'mysql+pymysql://{username}:{password}@{db_host}/{db_name}'.format(
     db_host = os.getenv('DB_HOST'),
     db_name = os.getenv('DB_NAME')
 )
-
-
 engine = create_engine(db_uri)
 connection = engine.connect()
 metadata = MetaData()
 
 data_table = Table('data-scraping', metadata, autoload=True, autoload_with=engine)
+
+
+
+def addDucks(ducks):
+    print(ducks['duck_name'])
+    print(ducks['duck_price'])
+    print(ducks['duck_image'])
+    ins = data_table.insert().values(duck_name = ducks['duck_name'], duck_price=ducks['duck_price'], duck_image=ducks['duck_image'], duck_url=ducks['duck_url'])
+    connection.execute(ins)
--- scrape.py
+++ scrape.py
@@ -18,17 +18,36 @@
 
 
 from bs4 import BeautifulSoup
-from db_connection import data_table, connection
+from db_connection import data_table, connection, addDucks
 import requests
 
 
-duck_website = 'https://shop.justducks.co.uk/characters-2?limitstart=0?limitstart=0?limitstart=0?limitstart=0&limit=0'
+duck_website = 'https://shop.justducks.co.uk/characters-2?limitstart=0?limitstart=0?limitstart=0?limitstart=0&limit=0?limitstart=0?limitstart=0?limitstart=0?limitstart=0&limit=0?limitstart=0?limitstart=0?limitstart=0?limitstart=0&limit=0?limitstart=0?limitstart=0?limitstart=0?limitstart=0&limit=0'
 source = requests.get(duck_website).text
 soup = BeautifulSoup(source, 'lxml')
+duck_shop_entry = 'https://shop.justducks.co.uk'
 
+for duck in soup.find_all('div', class_='hikashop_product'):
+    try:
+        duck_image = duck.find('picture').img['src']
+        duck_image = '{duck_shop_url}{duck_image}'.format(duck_shop_url = duck_shop_entry , duck_image=duck_image)
+        # print(duck_image)
+        duck_price = duck.find('span', class_="hikashop_product_price").text
+        duck_price = duck_price.split(' ')[1]
 
-main_content = soup.find('div', class_='hikashop_product')
-print(main_content)
+        duck_meta = duck.find('span', class_='hikashop_product_name').a
+        duck_name = duck_meta.text.strip()
+        duck_url = '{duck_shop_url}{duck_url}'.format(duck_shop_url=duck_shop_entry, duck_url=duck_meta['href'])
+        ducks = {
+            'duck_name':duck_name,
+            'duck_price':duck_price,
+            'duck_image':duck_image,
+            'duck_url':duck_url
+        }
+        print(duck_price)
+        addDucks(ducks)
+    except:
+        pass
 
 
 
